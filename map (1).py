d467477af37f594ad30914466ac04e14dba15aa6
MaMaLi2007 <149883445+MaMaLi2007@users.noreply.github.com>
2026-01-14 11:06:13 +0100

    Add files via upload

---
+++ map (1).py
@@ -0,0 +1,168 @@
+from algoviz.svg import SVGView, Circle, Rect, Text, Image
+from random import randrange
+import random
+
+class Map:
+
+    def __init__(self, length, height):
+        self.layout = [ [1 for col in range(height)] for row in range(length)] 
+        self.height = height  # arrayhöhe
+        self.length = length  # arraybreite
+        self.walls = []
+        
+
+    def get_length(self):
+        return self.length
+
+    def get_height(self):
+        return self.height
+
+    def get_layout(self):
+        return self.layout
+
+    
+    def set_length(self, length):
+        self.length = length
+
+    def set_height(self, height):
+        self.height = height
+        
+    def set_layout(self, layout):
+        self.layout = layout
+    
+    def create_maze(self, gegneranzahl):
+        # random punkte
+        ax = []
+        ay = []
+        for i in range(self.length * 2):
+            ax.append(random.randint(1, self.length - 2))
+            ay.append(random.randint(1, self.length - 2))
+
+        # verbinden der random punkte durch tunnel
+        while len(ax) >= 2:
+            self.create_tunnel(ax[0], ay[0], ax[1], ay[1])
+            for i in range (2):
+                ax.pop(0)
+                ay.pop(0)
+
+        # mitte frei
+        mx = self.get_height() // 2
+        my = self.get_length() // 2
+
+        self.layout[mx][my] = 0
+        self.layout[mx + 1][my] = 0
+        self.layout[mx][my + 1] = 0
+        self.layout[mx + 1][my + 1] = 0
+
+        # gegnerfelder
+        self.add_gegner(gegneranzahl, mx, my)
+
+        # items
+        self.add_items(random.randint(1,2), mx, my)
+
+    
+    # Verbindet zwei Punkte mit einem Weg
+    def create_tunnel(self, x1, y1, x2, y2):
+        self.layout[x1][y1] = 0
+        
+        while x1 != x2:
+            if x1 < x2:
+                x1 += 1
+                self.layout[x1][y1] = 0
+            else:
+                x1 -= 1
+                self.layout[x1][y1] = 0
+                
+        while y1 != y2:
+            if y1 < y2:
+                y1 += 1
+                self.layout[x1][y1] = 0
+            else:
+                y1 -= 1
+                self.layout[x1][y1] = 0
+                
+
+    # Fügt Gegener in die Map ein, an der Stelle, wo das Array den Wert zwei hat, erstellt
+    def add_gegner(self, gegneranzahl, mx, my):
+        while gegneranzahl > 0:
+            x = random.randint(1, self.length - 1)
+            y = random.randint(1, self.length - 1)
+
+            if (self.layout[x][y] == 0) and ((x != mx) and (x != mx + 1)) and ((y != my) and (y != my + 1)):
+                gegneranzahl -= 1
+                self.create_tunnel(x, y, mx, my)
+                self.layout[x][y] = 2
+                
+
+    # Fügt Items in die Map ein, and er Stelle, wo Array den Wert drei hat
+    def add_items(self, itemanzahl, mx, my):
+        while itemanzahl > 0:
+            x = random.randint(1, self.length - 1)
+            y = random.randint(1, self.length - 1)
+
+            if (self.layout[x][y] == 0) and ((x != mx) and (x != mx + 1)) and ((y != my) and (y != my + 1)):
+                self.layout[x][y] = 3
+                itemanzahl -= 1
+            
+
+    # hardcoded lobby
+    def create_lobby(self):
+        self.layout =  [
+            [1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 3, 0, 0, 0, 0, 0, 1],
+            [1, 0, 1, 1, 0, 1, 0, 1],
+            [1, 0, 1, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 1],
+            [1, 2, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+
+    
+    # hardcoded endraum 
+    def create_end(self):
+        self.layout =[
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1],
+            [1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        ]
+
+
+    # zeichnet die wand
+    def zeichnen(self, view, l):
+        
+        w = l // self.length        # pixelbreite der wände
+        h = l // self.height        # pixelhöhe der wände
+        
+        for i in range(self.length):
+            for j in range(self.height):
+                if self.layout[j][i] == 1:
+                    # rect = Rect(i * w, j * h, w, h, view)
+                    # rect.set_fill("grey")
+                    # rect.set_stroke_width(3)
+                    wall = Image("Wall.png", i * w, j * h, w, h, view)
+                    self.walls.append(wall)
+                    
+                #if self.layout[j][i] == 2:
+                    # rect = Rect(i * w, j * h, w, h, view)
+                    # rect.set_fill_rgb(255, 0, 0, 0.5)
+                    # rect.set_stroke_width(1)
+                    
+                #if self.layout[j][i] == 3:
+                    #rect = Rect(i * w, j * h, w, h, view)
+                    #rect.set_fill_rgb(255, 255, 0, 0.5)
+                    #rect.set_stroke_width(1)
+                    
+                
